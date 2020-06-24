4e268b15519112a3d3661d3d606ba16ced9585e1
unknown <alexis.platino@gmail.com>
2020-06-25 00:26:21 +0200

    App finished

--- src/statics/load-bar/index.js
+++ src/statics/load-bar/index.js
@@ -2,3 +2,23 @@ const progressBar = document.querySelector("#fill");
 const loadingText = document.querySelector("#loadingText");
 const mainText = document.querySelector("#main-text");
 const progressContainer = document.querySelector("#container");
+
+const ipcRenderer = require("electron").ipcRenderer;
+ipcRenderer.on("downloading-update", (ev, percentage) => {
+    const formatted = `${Math.round(percentage)}%`;
+    progressBar.style.width = formatted;
+    mainText.innerText = formatted;
+});
+
+ipcRenderer.on("no-updates", (ev) => {
+    mainText.innerText = "No updates available";
+})
+
+ipcRenderer.on("update-available", ev => {
+    progressContainer.style.display = "block";
+    loadingText.innerText = "Downloading"
+})
+
+ipcRenderer.on("update-not-found", () => {
+    mainText.innerText = "Update not found";
+})
